aa04e919091f0d47a42a68f9f184cfe23aa4137d
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-04-24 16:52:16 -0500

    01-Tipos-Basicos Codigo fuente

---
+++ Bases_TypeScript/Introduccion_TS/src/01-Tipos-Basicos/index.ts
@@ -0,0 +1,14 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+/* === Tipos de datos básicos === */
+let nombre: string = 'Laura';
+nombre = 'Laura Sofia';
+let aprendiendo: boolean = true;
+const apellido = 'Dueñas';
+let edad: number | string;
+edad = 22;
+edad = 'Veintidos años';
+
+
